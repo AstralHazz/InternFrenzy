5f5b22bfe8e75a984517920284e2e1095b5753bf
AstralHazz <bjaumaha@stedwards.edu>
2022-04-12 04:10:14 -0500

    Final Quest System
    I am in the process of adding the quests that Riley made for the game into the system. I couldn't get much done, though since i'm writing this @5AM

---
+++ Assets/Scripts/MainMenu_Script.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Script : MonoBehaviour
+{
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene("Bigger Toy"); //Put name of main scene in here
+    }
+/*
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void PlayFinalLevel()
+    {
+        SceneManager.LoadScene("LastLevel");
+    }
+*/
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("IntroMenu");
+    }
+}
